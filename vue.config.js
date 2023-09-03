f397fd745bba0a91cde318c9a7e90499b5597d54
dxkite <dxkite@163.com>
2023-09-03 12:20:11 +0800

    feat: 调整代理

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api': {
-          target: 'http://localhost:8080/', // 真实请求UR
+          target: 'http://127.0.0.1:8080/', // 真实请求UR
           changeOrigin: true, // 允许跨域
         }
       }
