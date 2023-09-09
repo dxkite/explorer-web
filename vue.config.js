573c9e8d2e0159ad199c39026c3dc2230cabdc30
dxkite <dxkite@163.com>
2023-09-09 16:01:55 +0800

    feat: 新增包分析器

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://127.0.0.1:8080/', // 真实请求UR
-          changeOrigin: true, // 允许跨域
-        }
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080/', // 真实请求UR
+        changeOrigin: true, // 允许跨域
       }
     }
+  },
+  chainWebpack: config => {
+    // 配置包分析器
+    config.plugin('webpack-bundle-analyzer')
+      .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
   }
+}
