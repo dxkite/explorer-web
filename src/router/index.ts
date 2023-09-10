17087ad9a1d4559cf456e14c561fde78b97c622f
dxkite <dxkite@163.com>
2023-09-10 18:29:26 +0800

    feat: 新增404目录

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const routes: Array<RouteConfig> = [
     path: '/p/:path(.*)',
     name: 'Path',
     component: Home
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: Home
   }
 ]
 
