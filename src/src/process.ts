d43d8e15c3d5f3662288ced7d6c71b6bf1806867
dxkite <dxkite@163.com>
2023-09-23 19:14:15 +0800

    feat: 调整进度条

--- src/src/process.ts
+++ src/src/process.ts
@@ -2,14 +2,13 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
 NProgress.configure({
-  easing: 'ease', // 动画方式
-  speed: 1000, // 递增进度条的速度
-  showSpinner: false, // 是否显示加载ico
-  trickleSpeed: 200, // 自动递增间隔
-  minimum: 0.3, // 更改启动时使用的最小百分比
-  parent: 'body', //指定进度条的父容器
+  easing: 'ease',
+  speed: 100,
+  showSpinner: false,
+  trickleSpeed: 10,
+  minimum: 0.1,
+  parent: '#app',
 })
-
 
 const axiosLoadingUrls: string[] = []
 
